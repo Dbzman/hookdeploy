f14ffa8962dd733d8df14035ea7a16554f0cb34b
Timo Litzius <dev@dbzman-online.eu>
2016-04-10 00:46:49 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$currentDeployKey = $_GET['key'];
+
+if (is_string($currentDeployKey) && strlen($currentDeployKey) > 0) {
+    $config = json_decode(file_get_contents("config.json"));
+
+    foreach ($config as $configItem) {
+        $app = $configItem[0];
+        $name = $app->name;
+        $deployKey = $app->deployKey;
+        $deployScript = $app->deployScript;
+
+        if ($deployKey == $currentDeployKey) {
+            $commandReturn = 0;
+            $output = system($deployScript . " 2>&1", $commandReturn);
+            if ($commandReturn === 0) {
+                echo "[deployment succeeded]";
+            } else {
+                echo "[deployment failed]";
+            }
+        }
+    }
+}
+
