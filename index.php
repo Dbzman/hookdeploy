f2d4f732a589b251c0c9f082262a611018c2007a
Timo Litzius <dev@dbzman-online.eu>
2016-04-10 01:01:09 +0200

    Add ability to execute in a working dir

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ if (is_string($currentDeployKey) && strlen($currentDeployKey) > 0) {
     foreach ($config as $configItem) {
         $app = $configItem[0];
         $name = $app->name;
+        $workingDir = $app->workingDir;
         $deployKey = $app->deployKey;
         $deployScript = $app->deployScript;
 
         if ($deployKey == $currentDeployKey) {
             $commandReturn = 0;
-            $output = system($deployScript . " 2>&1", $commandReturn);
+            $output = system("cd $workingDir && $deployScript 2>&1", $commandReturn);
             if ($commandReturn === 0) {
                 echo "[deployment succeeded]";
             } else {
